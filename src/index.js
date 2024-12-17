b4c953d5ad60520219eb4ce2d9a35c6990b85355
grichard99 <guy.richard@summerathletes.com>
2024-12-16 23:45:11 -0600

    updating nil/null handling

--- src/index.js
+++ src/index.js
@@ -106,6 +106,14 @@ exports.format = (amount, currency) => {
 
 //Adding commas to the amount
 const formatAmount = (amount, decimalPlaces) => {
-  // If amount = 3250 and decimalPlaces = 2, this will return "3,250.00"
-  return amount.toFixed(decimalPlaces).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  // If amount is undefined, return 0 with appropriate decimal places
+  if (!amount) {
+    let nilPlaceHolder = 0;
+    return nilPlaceHolder
+      .toFixed(decimalPlaces)
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  } else {
+    // If amount = 3250 and decimalPlaces = 2, this will return "3,250.00"
+    return amount.toFixed(decimalPlaces).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
 };
